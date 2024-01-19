5953989a34732e3bc65bd0b59c88f7b7433942b3
lukasenko <53124868+lukasplevko@users.noreply.github.com>
2024-01-19 20:43:50 +0100

    Update caesar_cipher.rb
    Added comments

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -4,8 +4,11 @@ def caesar_cipher(word,increment)
 		character = char;
 		is_upcase = character == character.upcase
 		if(char.to_i(base) != 0)
+			#convert character in word to base36 number
 			number = char.to_i(base);
+			#increments the number, if it reaches end it starts from 10 which represents A in base36
 			increment.times { |t| number + 1 >= base ? number = 10 : number+=1 }
+			#convert back to character
 			character = number.to_s(base)
 		end
 		is_upcase ? character.upcase : character
